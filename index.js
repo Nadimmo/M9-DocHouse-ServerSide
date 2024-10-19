16aa68d502cc1245a67ac60e1294fc39a892adee
nadimmostafa <nadimmostafa334@gmail.com>
2024-10-19 15:37:07 +0600

    add doctor and review related api

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+const port =  process.env.PORT || 5000;
+
+// Middleware to parse JSON request bodies
+
+app.use(express.json());
+app.use(
+    cors({
+      origin: ["http://localhost:5173"],
+    })
+  );
+
+
+  
+  
+const { MongoClient, ServerApiVersion, Db, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rrkijcq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// const uri = 'mongodb://localhost:27017/?directConnection=true'
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  const CollectionOfDoctors = client.db("DoctorsHouseDB").collection("DoctorsDB")
+  const CollectionOfReviews = client.db("DoctorsHouseDB").collection("ReviewDB")
+
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    // doctors api
+    app.get('/doctors',async (req,res)=>{
+      const doctor = req.body
+      const result = await CollectionOfDoctors.find(doctor).toArray()
+      res.send(result)
+    })
+
+    app.post('/doctors', async(req,res)=>{
+      const doctor = req.body
+      const result = await CollectionOfDoctors.insertOne(doctor)
+      res.send(result)
+    })
+
+    // review api
+    app.get('/reviews', async(req,res)=>{
+      const review = req.body
+      const result = await CollectionOfReviews.find(review).toArray()
+      res.send(result)
+    })
+
+    app.post('/reviews', async(req,res)=>{
+      const review = req.body
+      const result = await CollectionOfReviews.insertOne(review)
+      res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req,res)=>{
+    res.send("server is connecting")
+})
+
+app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`)
+})
+
